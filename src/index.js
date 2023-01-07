9096f9f2c31fbeffa146ce9a5fd3052a5e76c895
verter187 <verter187@inbox.ru>
2023-01-07 19:15:10 +0300

    adding a new commit front_20230107

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import './style/style.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+import "./style/style.scss";
 
+const marvelService = new MarvelService();
+// marvelService.getAllCharacters().then((res) => console.log(res));
+marvelService.getCharacter(1011052).then((res) => console.log(res));
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
