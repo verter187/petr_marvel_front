c5bc5e2aa831e11991f4a14a980c0cb6b48754cf
verter187 <verter187@inbox.ru>
2023-01-29 16:09:13 +0300

    new commit front_test_hooks_20230129

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -6,44 +6,78 @@ import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from "../../resources/img/vision.png";
 
-class App extends Component {
-  state = {
-    showRandomChar: true,
-    selectedChar: null,
+const App = () => {
+  const [showRandomChar, setShowRandomChar] = useState(true);
+  const [selectedChar, setSelectedChar] = useState(null);
+
+  const onCharSelected = (id) => {
+    setSelectedChar((selectedChar) => id);
   };
 
-  onCharSelected = (id) => {
-    this.setState({ selectedChar: id });
-  };
-  toggleRandomChar = () => {
-    this.setState((state) => {
-      return { showRandomChar: !state.showRandomChar };
-    });
+  const toggleRandomChar = () => {
+    setShowRandomChar((showRandomChar) => !showRandomChar);
   };
 
-  render() {
-    return (
-      <div className="app">
-        <AppHeader />
-        <main>
-          {this.state.showRandomChar ? (
-            <ErrorBoundary>
-              <RandomChar />
-            </ErrorBoundary>
-          ) : null}
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        {showRandomChar ? (
+          <ErrorBoundary>
+            <RandomChar />
+          </ErrorBoundary>
+        ) : null}
 
-          <button onClick={this.toggleRandomChar}>Click me</button>
-          <div className="char__content">
-            <CharList onCharSelected={this.onCharSelected} />
-            <ErrorBoundary>
-              <CharInfo charId={this.state.selectedChar} />
-            </ErrorBoundary>
-          </div>
-          <img className="bg-decoration" src={decoration} alt="vision" />
-        </main>
-      </div>
-    );
-  }
-}
+        <button onClick={toggleRandomChar}>Click me</button>
+        <div className="char__content">
+          <CharList onCharSelected={onCharSelected} />
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+};
+// class App extends Component {
+//   state = {
+//     showRandomChar: true,
+//     selectedChar: null,
+//   };
+
+//   onCharSelected = (id) => {
+//     this.setState({ selectedChar: id });
+//   };
+//   toggleRandomChar = () => {
+//     this.setState((state) => {
+//       return { showRandomChar: !state.showRandomChar };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="app">
+//         <AppHeader />
+//         <main>
+//           {this.state.showRandomChar ? (
+//             <ErrorBoundary>
+//               <RandomChar />
+//             </ErrorBoundary>
+//           ) : null}
+
+//           <button onClick={this.toggleRandomChar}>Click me</button>
+//           <div className="char__content">
+//             <CharList onCharSelected={this.onCharSelected} />
+//             <ErrorBoundary>
+//               <CharInfo charId={this.state.selectedChar} />
+//             </ErrorBoundary>
+//           </div>
+//           <img className="bg-decoration" src={decoration} alt="vision" />
+//         </main>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
